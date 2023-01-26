6bd770e554d9a9d2e62a45f3308b48754aa651e8
Oleg Suvorov <olegsuv@gmail.com>
2023-01-26 20:43:50 +0100

    Add react-photo-slider

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -4,6 +4,7 @@ import InputIcon from "@mui/icons-material/Input";
 import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
 import GridViewIcon from "@mui/icons-material/GridView";
 import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import CollectionsIcon from "@mui/icons-material/Collections";
 import { GetListItemButton } from "./GetListItemButton";
 
 export function Tasks() {
@@ -14,16 +15,20 @@ export function Tasks() {
       </Typography>
       <List component="nav">
         <GetListItemButton
+          URL="github.com/olegsuv/react-checkers"
+          Icon={GridViewIcon}
+        />
+        <GetListItemButton
+          URL="github.com/olegsuv/react-photo-slider"
+          Icon={CollectionsIcon}
+        />
+        <GetListItemButton
           URL="github.com/olegsuv/react-auto-complete"
           Icon={InputIcon}
         />
         <GetListItemButton
           URL="github.com/olegsuv/react-todo-list"
           Icon={FormatListBulletedIcon}
-        />
-        <GetListItemButton
-          URL="github.com/olegsuv/react-checkers"
-          Icon={GridViewIcon}
         />
         <GetListItemButton
           URL="github.com/olegsuv/jqTree-generator"
