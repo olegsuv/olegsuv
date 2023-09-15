71159ccabcfeaa970a42cd0908d52cd6c0121596
Oleg Suvorov <olegsuv@gmail.com>
2023-09-15 19:34:22 +0200

    Add copy field for messengers

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,16 +1,29 @@
 import Box from "@mui/material/Box";
 import * as React from "react";
-import { Button, Grid, Typography } from "@mui/material";
+import {
+  Alert,
+  Button,
+  FormControl,
+  Grid,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 import PhoneInput from "react-phone-input-2";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import ChatIcon from "@mui/icons-material/Chat";
-import { useState } from "react";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import Snackbar from "@mui/material/Snackbar";
+import { useRef, useState } from "react";
 import "react-phone-input-2/lib/style.css";
 
 export function Generator() {
   const [value, setValue] = useState();
-  const buttonProps = (backgroundColor) => ({
+  const [open, setOpen] = useState(false);
+  const textFieldRef = useRef<HTMLInputElement | null>(null);
+
+  const buttonProps = (backgroundColor: string) => ({
     disabled: !value,
     variant: "contained" as "contained",
     target: "_blank",
@@ -21,13 +34,27 @@ export function Generator() {
     },
   });
 
+  const onCopyClick = () => {
+    textFieldRef.current?.select();
+    navigator.clipboard
+      .writeText(window.location.href)
+      .then(() => {
+        setOpen(true);
+      })
+      .catch(() => {
+        alert(
+          "Your browser is not support clipboard API, please copy manually via CTRL+C"
+        );
+      });
+  };
+
   return (
     <>
       <Typography variant="h1" fontSize="3rem" color="primary" mt={4}>
         Messengers link generator
       </Typography>
       <Typography mt={2} mb={2}>
-        Enter your phone number:
+        1. Enter anyone's phone number:
       </Typography>
       <PhoneInput
         inputProps={{
@@ -40,7 +67,7 @@ export function Generator() {
         onChange={setValue as any}
       />
       <Typography mt={2} mb={2}>
-        Get you links:
+        2. Get your links:
       </Typography>
       <Grid container ml={-1} mt={-1}>
         <Box>
@@ -92,6 +119,39 @@ export function Generator() {
           </Button>
         </Box>
       </Grid>
+      <Typography mt={2} mb={2}>
+        3. Share page with friends:
+      </Typography>
+      <FormControl>
+        <TextField
+          inputRef={textFieldRef}
+          value={window.location.href}
+          onClick={onCopyClick}
+          sx={{
+            width: 300,
+          }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <ContentCopyIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </FormControl>
+      <Snackbar
+        open={open}
+        autoHideDuration={3000}
+        onClose={() => setOpen(false)}
+      >
+        <Alert
+          onClose={() => setOpen(false)}
+          severity={"success"}
+          sx={{ width: "100%" }}
+        >
+          Link {window.location.href} was copied to clipboard!
+        </Alert>
+      </Snackbar>
     </>
   );
 }
